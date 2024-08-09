76076f4bf34e70d4445c3cd38efff4a369e613fa
Andrei Vasilyev <andrei.vasilyev80@gmail.com>
2024-08-09 12:54:10 +0300

    added color

--- src/main/java/dto/Car.java
+++ src/main/java/dto/Car.java
@@ -1,9 +1,11 @@
 package dto;
 
+import java.awt.*;
 import java.time.LocalDate;
 
 public class Car {
     private String model;
     private String manufacturer;
     private LocalDate produced;
+    private Color color;
 }
